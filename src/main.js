0258efb206015f8d7d68ce052f97f8e98ced5b26
Suraj Pradhan <71517930+celestialsuraj@users.noreply.github.com>
2024-03-12 13:28:48 +0530

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store/store";
 
 // import the necessary css file
 import "vue-awesome-paginate/dist/style.css";
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).mount("#app");
